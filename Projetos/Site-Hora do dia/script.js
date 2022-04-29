8c7f84f7789498e0ca2c1f19fbf2f38feef99e1c
Vinicius Xavier <xavierviniciusxavier94@gmail.com>
2022-04-29 09:46:13 -0300

    Finalização

--- Projetos/Site-Hora do dia/script.js
+++ Projetos/Site-Hora do dia/script.js
@@ -3,13 +3,18 @@ function carregar() {
     var imagem = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+    var minutos = data.getMinutes()
     msg.innerHTML = `Agora são ${hora} horas `
+    
     if (hora >=0 && hora < 12) {
         imagem.src = 'Imagens/manha.png'
-    }else if(hora >= 12 && hora <18){
+        document.body.style.background = '#ffe973'
+    }else if(hora >= 12 && hora < 18){
         imagem.src = 'Imagens/tarde.png'
+        document.body.style.background = '#edb96d'
     }else{
         imagem.src = 'Imagens/noite.png'
+        document.body.style.background = '#182946'
     }
    
 }
