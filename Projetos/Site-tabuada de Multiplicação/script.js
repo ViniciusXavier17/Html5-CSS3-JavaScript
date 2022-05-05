838413c45faea44d39fc3eac118fa46d5cfbe0a8
Vinicius Xavier <xavierviniciusxavier94@gmail.com>
2022-05-05 13:58:36 -0300

    Projeto tabuada

---
+++ Projetos/Site-tabuada de Multiplicação/script.js
@@ -0,0 +1,22 @@
+function calcular(){
+    let numero = window.document.getElementById('txtnum')
+    let tab = window.document.getElementById('seltabu')
+    if(numero.value.length == 0){
+        window.alert('Campos não preenchidos')
+    }else{
+        let n = Number(numero.value)
+        tab.innerHTML = ''
+        for (let i = 0; i < 10;i++){
+            let item = window.document.createElement('option')
+            item.text = `${n} x ${i+1} = ${n*(i+1)}`
+            item.value = `tab${i}`
+            tab.appendChild(item)
+        }
+    }
+
+
+    
+
+
+
+}
